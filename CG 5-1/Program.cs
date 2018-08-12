0fd282cd5d09b7c980573c687ea34a63f45488b1
Melissa DoddRuesing <melissa.doddruesing@gmail.com>
2018-08-12 12:03:45 -0500

    full name method

--- CG 5-1/Program.cs
+++ CG 5-1/Program.cs
@@ -6,7 +6,35 @@ namespace CG_5_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your name?");
+            Console.Write("Type your first name: ");
+            string myFirstName;
+            myFirstName = Console.ReadLine();
+
+            //string myLastName;
+            //Console.Write("Type your last name: ");
+            //myLastName = Console.ReadLine();
+
+            Console.WriteLine("Type your last name: ");
+            string myLastName = Console.ReadLine();
+
+
+
+
+            Console.WriteLine("Hello " + fullName(myFirstName, myLastName));
+            Console.ReadLine();
+
+
+
+
+
         }
+        private static string fullName(string myFirstName, string myLastName)
+
+        {
+            return (myFirstName + " " + myLastName);
+        }
+
+
     }
 }
